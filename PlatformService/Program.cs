1e9ea6e4a82eb73c6710c58160bf1b7fd261c991
Si Lam <silam@hotmail.com>
2022-04-05 06:22:21 -0500

    Create AutoMapper from Platform model to ReadDTO

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -12,6 +12,9 @@ builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMe
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
 builder.Services.AddControllers();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c=>{
